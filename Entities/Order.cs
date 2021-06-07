fdbf8272e455463ea43f14d3dc0266697c097d64
441122229944 <davidtherion412294@gmail.com>
2021-06-06 22:37:44 -0300

    Nova versao

---
+++ Entities/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using projeto_teste.Entities.Enums;
+
+namespace projeto_teste.Entities {
+    class Order {
+        public  int Id { get; set; }
+        public  DateTime Moment { get; set; }
+        public  OrderStatus Status { get; set; }
+
+        public override string ToString() {
+            return "ID = "+Id
+                + ", Moment = "
+                + Moment
+                + ", Status = "
+                + Status;
+        }
+    }
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using projeto_teste.Entities;
+using projeto_teste.Entities.Enums;
 
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("New project");
-            Console.WriteLine("New project1");
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            
         }
     }
 }
